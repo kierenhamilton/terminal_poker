543ba3ebedb207da7c423f0ae415acff77483fd8
kierenhamilton <kierenhamilton1@outlook.com>
2025-07-03 01:50:01 +0100

    add player

--- src/poker.cpp
+++ src/poker.cpp
@@ -1,7 +1,7 @@
-#include <vector>
-#include "card_engine.h"
 #include "poker.h"
-
+#include "card_engine.h"
+#include <string>
+#include <vector>
 
 void Game::setup() {
   init_deck(deck);
@@ -9,11 +9,16 @@ void Game::setup() {
   for (int i = 0; i < 5; i++)
     hidden_cards.push_back(draw_card(deck));
 
-  for (int i = 0; i < 2; i++){
+  for (int i = 0; i < 2; i++) {
 
-    for (Player& player : players){
+    for (Player &player : players) {
       player.set_player_hand(draw_card(deck));
     }
   }
+}
+
+void Game::add_player(std::string name, int money) {
+  players.push_back(Player(name, money));
+  player_number++;
 }
 
